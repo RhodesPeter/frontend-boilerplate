4f763c004092cfd8b9a1a2774cf4cea6723129a0
peterrhodes <rhodespeter@hotmail.co.uk>
2018-01-07 22:12:33 +0000

    Add Grunt - used to compile scss
    - todo add postCSS ti gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+        options: {
+          sourcemap: 'none'
+        },
+				files: {
+					'public/build/main.css' : 'scss/main.scss'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: 'scss/*.scss',
+				tasks: ['sass']
+			}
+		}
+  });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default',['watch']);
+};
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 #### scripts:
 
+[![Built with Grunt](https://cdn.gruntjs.com/builtwith.svg)](https://gruntjs.com/)
+
 `npm run webpack` to bundle JS modules and pass through babel.
 
 `npm run watchCss` to compile CSS from Sass and pass through postCSS.
